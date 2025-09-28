143237d5e872f10c6cf18aab3c4236c92739a4b3
Vokpolk <alexanderklopkov@gmail.com>
2025-09-28 17:34:51 +0300

    Initial Commit

---
+++ Travel Shedule/Travel_SheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_SheduleApp.swift
+//  Travel Shedule
+//
+//  Created by Александр Клопков on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Travel_SheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
